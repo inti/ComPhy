7cd7a2bfba1901d7f450fc4ca78ade835cdede63
Inti Pedroso <intipedroso@gmail.com>
2012-04-24 16:38:54 +0100

    First Perl based version Implemented - script to build DB for seq id to tax id. This saves times when reading the files - basic function for diff misc tasks on PhyloStratiphytUtils.pm - beginning of main script
    Signed-off-by: Inti Pedroso <intipedroso@gmail.com>

---
+++ PhyloStratiphytUtils.pm
@@ -0,0 +1,181 @@
+package PhyloStratiphytUtils;
+use strict;
+use warnings;
+use Carp;
+use Exporter qw (import);
+
+eval { 
+    use DB_File;
+    use Fcntl;
+    use Data::Dumper;
+};
+if ($@) { 
+	print "Some libraries does not seem to be in you system. quitting\n";
+	exit(1);
+}
+
+our (@EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+@EXPORT = qw( parse_gi_taxid_files print_OUT build_database progress_bar read_taxonomy_files);				# symbols to export by default
+@EXPORT_OK = qw( parse_gi_taxid_files print_OUT build_database progress_bar read_taxonomy_files);			# symbols to export on request
+
+sub read_taxonomy_files {
+    my $tax_folder = shift;
+    my (%tax_info_db, %tax_tree_db);
+    
+    print_OUT("   '-> Working on information linked to species id");
+    # first work on the species names.
+    print_OUT("   '-> [ names.dmp ]");
+    open (DMP,"$tax_folder/names.dmp") or die $!;
+    my $n_lines = get_file_number_of_lines("$tax_folder/names.dmp");
+    my $counter = 0;
+    while (my $line = <DMP>) {
+        print scalar localtime," \t", progress_bar(++$counter,$n_lines);
+        chomp($line);
+        $line =~ s/\t+//g; 
+        $line =~ s/|$//; 
+        my ($tax_id, $name_txt,$unique_name,$name_class) = split(/\|/,$line);
+        if (not defined $tax_info_db{$tax_id}) {
+            $tax_info_db{$tax_id} = {   'synonym' => [], 
+                'name_txt' => "",
+                'rank' => "",
+                'embl_code' => "",
+                'division_id' => "",
+                'comments' => ""
+            };
+            
+        }
+        if ( $name_class ne 'scientific name') { 
+            @{ $tax_info_db{$tax_id}->{'synonym'} } = $name_txt; 
+            next;
+        }
+        $tax_info_db{$tax_id}->{'name_txt'} = $name_txt;
+        if ($unique_name eq ''){ 
+            $tax_info_db{$tax_id}->{$name_class} = $name_txt;
+        } else {
+            $tax_info_db{$tax_id}->{$name_class} = $unique_name;
+            push @{ $tax_info_db{$tax_id}->{'synonym'} }, $name_txt;
+        }
+    }
+    print "\n"; # new line for to finish the progress bar.
+    
+    
+    #####
+    # now work on the taxonomy itself.
+    print_OUT("   '-> Working on taxnomy tree");
+    # create database for the taxonomy tree
+    open (DMP,"$tax_folder/nodes.dmp") or die $!;
+    print_OUT("   '-> [ nodes.dmp ]");
+    $n_lines = get_file_number_of_lines("$tax_folder/nodes.dmp");
+    $counter = 0;
+    while (my $line = <DMP>) {
+        print scalar localtime," \t", progress_bar(++$counter,$n_lines);
+        chomp($line);
+        $line =~ s/\t+//g; 
+        $line =~ s/|$//; 
+        my ($tax_id,                        #-- node id in GenBank taxonomy database
+        $parent_tax_id,                  #-- parent node id in GenBank taxonomy database
+        $rank,                           #-- rank of this node (superkingdom, kingdom, ...) 
+        $embl_code,                      #-- locus-name prefix; not unique
+        $division_id,                    #-- see division.dmp file
+        $inherited_div_flag,             #(1 or 0)		-- 1 if node inherits division from parent
+        $genetic_code_id,				#-- see gencode.dmp file
+        $inherited_GC_flag,              #(1 or 0)		-- 1 if node inherits genetic code from parent
+        $mitochondrial_genetic_code_id,	#-- see gencode.dmp file
+        $inherited_MGC_flag,             #(1 or 0)		-- 1 if node inherits mitochondrial gencode from parent
+        $GenBank_hidden_flag,            #(1 or 0)            -- 1 if name is suppressed in GenBank entry lineage
+        $hidden_subtree_root_flag,       #(1 or 0)       -- 1 if this subtree has no sequence data yet
+        $comments                       #-- free-text comments and citations 
+        )= split(/\|/,$line);
+        
+        # store child parent relationship
+        $tax_tree_db{$tax_id} = { 'parent' => $parent_tax_id };
+        # store adition information on the tree.
+        $tax_info_db{$tax_id}->{'rank'} = $rank;
+        $tax_info_db{$tax_id}->{'embl_code'} = $embl_code;
+        $tax_info_db{$tax_id}->{'division_id'} = $division_id;
+        $tax_info_db{$tax_id}->{'comments'} = $comments;
+    }
+    print "\n"; # finish progress bar.
+    return(\%tax_info_db,\%tax_tree_db);
+}
+
+sub build_database {
+    print_OUT("Building database with taxonomy information");
+    my $seq_to_tax_db_name = shift;
+    my $tax_folder = shift;
+    my $seq_id_files_to_parse = shift;
+
+    $seq_id_files_to_parse ||= "both";
+    print_OUT("   '-> Working on sequence id to species id mapping");
+    # CREATE HAShes to store data
+    # create the db on disk.
+    print_OUT("Creating DBs on disk");
+    
+    my %seq_to_tax_db;
+    
+    my $SEQ = tie %seq_to_tax_db, "DB_File", "$seq_to_tax_db_name.db", O_RDWR|O_CREAT, 0640 or die "Cannot open file to create db [ $seq_to_tax_db_name ]: $!\n";
+    
+    # set the sequences id to taxonomy id database.
+    my $seq_id_files = [];
+    if ($seq_id_files_to_parse eq 'both') {
+        $seq_id_files = ["$tax_folder/gi_taxid_nucl.dmp","$tax_folder/gi_taxid_nucl.dmp"];
+    } elsif ($seq_id_files_to_parse eq 'nucl') {
+        $seq_id_files = ["$tax_folder/gi_taxid_nucl.dmp"];        
+    } elsif ($seq_id_files_to_parse eq 'prot') {
+        $seq_id_files = ["$tax_folder/gi_taxid_prot.dmp"];        
+    }
+    # read file and store the mapping between ids.
+    foreach my $f (@$seq_id_files){
+        print_OUT("   '-> [ $f ]");
+        open (DMP,$f) or die $!;
+        my $n_lines = get_file_number_of_lines($f);
+        my $type;
+        $type = 'prot' if ($f eq "$tax_folder/gi_taxid_prot.dmp");
+        $type = 'nucl' if ($f eq "$tax_folder/gi_taxid_nucl.dmp");
+        while (my $line = <DMP>) {
+            print scalar localtime," \t", progress_bar($.,$n_lines);
+            chomp($line);
+            my($gi,$tax_id) = split(/[\s+\t+]/,$line);
+            $seq_to_tax_db{$gi} = $tax_id; #{' tax_id' => $tax_id, type => ""};
+        }
+        close(DMP);
+        print "\n";
+    }
+    
+    return(%seq_to_tax_db);
+}
+
+# wget-style. routine by tachyon
+# at http://tachyon.perlmonk.org/
+sub progress_bar {
+    my ( $got, $total, $width, $char ) = @_;
+    $width ||= 25; $char ||= '=';
+    my $num_width = length $total;
+    sprintf "   |%-${width}s| Done with [ %${num_width}s ] of [ %s (%.2f%%) ]\r", 
+    $char x (($width-1)*$got/$total). '>', 
+    $got, $total, 100*$got/+$total;
+}
+
+
+sub get_file_number_of_lines {
+    my $name = shift;
+    my $wc_output = `wc -l $name`;
+    my @n = split(/\s+/,$wc_output);
+    return($n[1]);
+}
+
+
+sub print_OUT {
+	my $string = shift;
+	my @file_handles = @_; 	
+	print scalar localtime(), "\t$string\n";
+	unless (scalar @file_handles == 0){
+		foreach my $fh (@file_handles){
+			print $fh scalar localtime(), "\t$string\n";
+		}
+	}
+}
+
+
+1;
--- phylostratiphy.pl
+++ phylostratiphy.pl
@@ -1,5 +1,142 @@
 #!/usr/bin/perl -w
 use strict;
+use Bio::TreeIO; 
+use Bio::DB::Taxonomy;
+use Bio::Phylo::IO 'parse';
+use Data::Dumper;
+use Getopt::Long;
+use Pod::Usage;
+use PDL;
+use MLDBM qw(DB_File Storable);
+use Fcntl;
+
+
+# local modules
+
+use PhyloStratiphytUtils;
+
+our (   $help, $man, $tax_folder, $blast_out, $blast_format,
+        $nucl_only, $prot_only, $seq_to_gi, $tax_info, $use_coverage);
+
+GetOptions(
+    'help' => \$help,
+    'man' => \$man,
+    'blast=s@' => \$blast_out,
+    'tax_folder=s' => \$tax_folder,
+    'blast_format=s' => \$blast_format,
+    'nucl_only' => \$nucl_only,
+    'prot_only' => \$prot_only,
+    'seq_to_gi=s' => \$seq_to_gi,
+    'tax_info=s' => \$tax_info,
+    'use_coverage' => \$use_coverage,
+) or pod2usage(0);
+
+pod2usage(0) if (defined $help);
+pod2usage(-exitstatus => 2, -verbose => 2) if (defined $man);
+
+$seq_to_gi ||= "seq_to_gi";
+$tax_info ||= "tax_info";
+
+print_OUT("Parsing taxonomy information");
+
+my $seq_id_files = 'both';
+if ($nucl_only) {
+    $seq_id_files = "nucl";
+    print_OUT("Only working with protein sequences");
+} elsif ($prot_only) {
+    $seq_id_files = "prot";        
+    print_OUT("Only working with nucleic acid sequences");
+}
+print_OUT("Mapping sequence ids to taxonomy ids");
+
+my (%seq_to_tax_db,%tax_info_db,%tax_tree_db);
+if (defined $tax_folder){
+    print_OUT("Openning databases");
+    print_OUT("   '-> [ $tax_folder/$seq_to_gi.db ]");
+
+    tie %seq_to_tax_db, "DB_File", "$tax_folder/$seq_to_gi.db" or die "Cannot open db file [ $tax_folder/$seq_to_gi.db ]: $!\n";
+    
+} else {
+    print_OUT("Taxonomy information will downloaded and stored on folder [ data ]");
+    $tax_folder = "data";
+    %seq_to_tax_db  = build_database($seq_to_gi,$tax_folder,$seq_id_files);   
+}
+
+print_OUT("   '-> Reading phylogenetic tree and species information");
+my $nodesfile = $tax_folder . "nodes.dmp";
+my $namefile = $tax_folder . "names.dmp";
+my $db = Bio::DB::Taxonomy->new(-source => 'flatfile', -nodesfile => $nodesfile, -namesfile => $namefile);
+
+#map { 
+#    print $_,"\t", $seq_to_tax_db{$_},"\n";
+#    my $taxon =   $db->get_taxon(-taxonid => $seq_to_tax_db{$_});
+#    print print "id is ", $taxon->id, "\n"; # 9606
+#    print "rank is ", $taxon->rank, "\n"; # species
+#    print "scientific name is ", $taxon->scientific_name, "\n"; # Homo sapiens
+#    print "division is ", $taxon->division, "\n"; # Primates
+#    getc; 
+#} keys %seq_to_tax_db;
+
+
+### define some variables to start storing the results
+
+my %S = (); # hash will store to score for each species.
+
+
+print_OUT("Starting to parse blast output");
+$seq_to_tax_db{"322792145"} = 13686;
+foreach my $file (@$blast_out){
+    #print_OUT("   '-> [ $file ]");
+    open (FILE,$file)or die $!;
+    my %fields = ();
+    while (my $line = <FILE>){
+        if ($line =~ m/^#/){
+            if ($line =~ m/^# Fields:/){
+                # clean up a bit the line
+                $line =~ s/^# Fields: //;
+                $line =~ s/\s$//;
+                $line =~ s/\.//g;
+                $line =~ s/\%/percent/g;
+                # split the fields names on the comas.
+                my @fs = split(/\,\s/,$line);
+                # store the field names and positions on a hash
+                for (my $i = 0; $i < scalar @fs; $i++) {
+                    $fs[$i] =~ s/\s+/_/g;
+                    $fields{$fs[$i]} = $i;
+                }
+            }
+            next;
+        }
+        chomp($line);
+        my @data = split(/\t+/,$line);
+        # remove the frame of the db hit, we only need to know about the frame of the query seq
+        if (exists $fields{'query/sbjct_frames'}){
+            $data[ $fields{'query/sbjct_frames'} ] =~ s/\/\w+$//;
+        }
+#print join "\n", keys %fields,"\n";
+#        print $data[ $fields{'subject_id'}],"\n";
+        my @subject_id = split(/\|/,$data[ $fields{'subject_id'}]);
+        my $coverage = 1;
+        if (defined $use_coverage) {
+            $coverage = ($data[ $fields{'s_end'}] - $data[ $fields{'s_start'}])/$data[ $fields{'query_length'}]; 
+        }
+        next if (not defined $seq_to_tax_db{$subject_id[1]});
+        $S{ $seq_to_tax_db{$subject_id[1]} } =+ (-log ($data[ $fields{'evalue'}])) * $coverage;
+
+
+#        print $subject_id[1],"\t",$seq_to_tax_db{$subject_id[1]},"\n";
+#        my $taxon =   $db->get_taxon(-taxonid => $seq_to_tax_db{$subject_id[1]});
+#        print "id is ", $taxon->id, "\n"; # 9606
+#        print "rank is ", $taxon->rank, "\n"; # species
+#        print "scientific name is ", $taxon->scientific_name, "\n"; # Homo sapiens
+#        print "division is ", $taxon->division, "\n"; # Primates
+        # add hit score to the target specie
+    }
+}
+
+print Dumper(%S);
+
+print_OUT("Done");
 
 
 exit;
