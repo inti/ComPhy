83e33ff5469b2bd3505b8b34a7574a75018c498c
Inti Pedroso <intipedroso@gmail.com>
2012-05-01 14:26:42 +0100

    major changes

--- get_viral_ids.pl
+++ get_viral_ids.pl
@@ -13,25 +13,26 @@ my $namefile = $tax_folder . "names.dmp";
 my $db = Bio::DB::Taxonomy->new(-source => 'flatfile', -nodesfile => $nodesfile, -namesfile => $namefile);
 
 my $taxon= $db->get_taxon(10239);
-
+print_OUT("Starting to identify viral taxa");
 my %all_viral_taxa = ();
 my @taxons = $db->each_Descendent($taxon);
 do {
-	print scalar localtime,"\t","Descendent [ ", scalar @taxons," ] and total [ ",scalar (keys %all_viral_taxa ) ," ]\n";
 	my @new_taxons = ();
 	foreach my $taxa (@taxons) {
-		print $taxa->scientific_name,"\n";
-		print "\thas as descendents :";
-		foreach my $new_taxa ( $db->each_Descendent($taxa) ) {
-			print "\t",$new_taxa->scientific_name,"\n"
-		}
 		push @new_taxons, $db->each_Descendent($taxa);
 	}
 	@taxons = ();
 	@taxons = @new_taxons;
 } until (scalar @taxons == 0);
 
-print_OUT("Done");
+print_OUT("Printing out");
 
+open (OUT,">$ARGV[1]") or die $!;
+foreach my $tx (keys %all_viral_taxa){
+	print OUT $tx->id,"\t",$tx->scientific_name,"\n";
+}
+close(OUT);
+
+print_OUT("Done");
 
 exit;
