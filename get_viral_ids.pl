f59002395d06d12497db25c5aa99f486b9084153
Inti Pedroso <intipedroso@gmail.com>
2012-07-10 11:02:33 +0100

    Minor tidy up
    Signed-off-by: Inti Pedroso <intipedroso@gmail.com>

--- get_viral_ids.pl
+++ get_viral_ids.pl
@@ -42,28 +42,5 @@ for my $child ( @extant_children ) {
     print OUT $child->ncbi_taxid,",",$child->id, ",", $child->rank, ",",$child->scientific_name, "\n";
 }
 close(OUT);
-exit;
-
-
-my $taxon= $db->get_taxon(10239);
-print_OUT("Starting to identify viral taxa");
-my %all_viral_taxa = ();
-my @taxons = $db->each_Descendent($taxon);
-do {
-	my @new_taxons = ();
-	foreach my $taxa (@taxons) {
-		push @new_taxons, $db->each_Descendent($taxa);
-	}
-	@taxons = ();
-	@taxons = @new_taxons;
-} until (scalar @taxons == 0);
-
-
-foreach my $tx (keys %all_viral_taxa){
-	print OUT $tx->id,"\t",$tx->scientific_name,"\n";
-}
-close(OUT);
-
 print_OUT("Done");
-
 exit;
