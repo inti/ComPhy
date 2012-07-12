4fff0bef301d6285894bdf437f63c824abd121ba
Inti Pedroso <intipedroso@gmail.com>
2012-07-12 14:48:01 +0100

    added dysbindin data

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 # run test of script.
 
-my_perl phylostratiphy.pl \
+perl phylostratiphy.pl \
     -blast blastp__Aech_v3.8.pep__nr.e10.sw.txt_old \
-    -tax_folder data2/ \
+    -tax_folder data/ \
     -prot_only \
     -query_taxon 103372 \
-    -virus_list tmp.virus.txt
-
+    -virus_list tmp.virus.txt \
+    -out test_phylostratiphy.txt
