777f5f6ec9533cefdd36c062d99dccd50bcac593
Inti Pedroso <intipedroso@gmail.com>
2012-07-17 19:12:45 +0100

    added dysbindin.tax_info.cs

--- run2.sh
+++ run2.sh
@@ -2,10 +2,11 @@
 
 # run test of script.
 
-my_perl phylostratiphy.pl \
+perl phylostratiphy.pl \
     -blast dysbindin.blast_out.txt \
     -tax_folder data/ \
     -query_taxon 9606 \
+    -hard 1e-3 \
     -virus_list tmp.virus.txt \
     -out test_phylostratiphy
 
