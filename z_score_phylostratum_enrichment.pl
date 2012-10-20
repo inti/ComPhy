818a9cd1b2b87179a08efac279ba7ce03ce62785
Inti Pedroso <intipedroso@gmail.com>
2012-10-21 00:06:43 +0100

    - improved documentation - added output with hard-scores per gene-set
    Signed-off-by: Inti Pedroso <intipedroso@gmail.com>

--- z_score_phylostratum_enrichment.pl
+++ z_score_phylostratum_enrichment.pl
@@ -73,8 +73,11 @@ my $phylostratum_scores = mpdl map {  $annotation{$_}->{scores};  } sort { $gene
 print_OUT("Calculating enrichment z-scores");
 my $phylostratum_totals = $phylostratum_scores->sumover->flat;
 my $N = $phylostratum_totals->sum;
-my $out_string = join "\t", ("GO",@phylostratum);
-$out_string .= "\n";
+my $out_string_z_score = join "\t", ("GO",@phylostratum);
+$out_string_z_score .= "\n";
+my $out_string_sets = join "\t", ("GO",@phylostratum);
+$out_string_sets .= "\n";
+
 foreach my $set (keys %gene_sets){
     my $profile = zeroes scalar @phylostratum;
     foreach my $gene (@{$gene_sets{$set}->{genes}}){
@@ -91,15 +94,23 @@ foreach my $set (keys %gene_sets){
         }
         push @z_scores, hygeometric_dist_z_score($N,$n,$R,$r);
     }
-    $out_string .= "$set\t" . join "\t", @z_scores;
-    $out_string .= "\n";
+    $out_string_z_score .= "$set\t" . join "\t", @z_scores;
+    $out_string_z_score .= "\n";
+    $out_string_sets .= "$set\t" . join "\t", $profile->list;
+    $out_string_sets .= "\n";
 }
 print_OUT("    ... done ...");
 
-print_OUT("Writting results to [ $out ]");
-open(OUT,">$out") or die $!;
-print OUT $out_string;
+print_OUT("Writting results to [ $out.z_score.txt ]");
+open(OUT,">$out.z_score.txt") or die $!;
+print OUT $out_string_z_score;
 close(OUT);
+
+print_OUT("Writting results to [ $out.hard_score.txt ]");
+open(OUT,">$out.hard_score.txt") or die $!;
+print OUT $out_string_sets;
+close(OUT);
+
 print_OUT("Finished");
 
 exit;
@@ -111,3 +122,79 @@ sub hygeometric_dist_z_score {
     my $z = ($r - $n*$R_over_N)/sqrt($n*($R_over_N)*(1-$R_over_N)*(1 - ($n - 1)/( $N - 1)));
     return($z);
 }
+
+
+__END__
+
+=head1 NAME
+
+ Calculation of enrichment scores of gene-sets of phylostratum.
+
+=head1 DESCRIPTION
+
+B<This program> will perform use hypegeometric distribution statistics to calculate the z-score enrichment statistic for enrichment of a gene-set among the genes of a phylostratum.
+
+Comments, suggestions or complains you be addressed to IntiPedroso@gmail.com
+
+=head1 SYNOPSIS
+
+    ./z_score_phylostratum_enrichment.pl -annot annotation_file -score_matrix phylostratiphy_score_matrix -out output_file_name
+
+    General options
+    -h, --help		print help message
+    -m, --man		print complete documentation
+
+    Input
+    -annot, -a      Gene annotation file.
+    -score_matrix, -m   Hard-score matrix output from phylostratigraphy.pl
+ 
+    Output
+    -out, --o      Name of output files
+
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-help>
+
+    Print help message
+
+=item B<-man>
+
+    Print complete documentation
+
+=item B<-annot, -a>
+
+    Gene annotation file. Format is tab separed with two columns. First column has the gene-set id and second column the gene-id.
+
+=item B<-score_matrix, -m>
+
+    Hard-score matrix output from phylostratigraphy.pl
+
+=item B<-o, --out>
+
+    Name of output file
+
+=back
+
+=head2 DESCRIPTION
+
+ The reported z-score corresponds to (Latex expression): z-sco\mbox{re}=\frac{r\; -\; n\frac{R}{N}}{\sqrt{n\; \cdot \; \left( \frac{R}{n} \right)\left( 1-\frac{R}{n} \right)\left( 1-\frac{n-1}{N-1} \right)}}
+ where R = is the total number of genes on the gene-set, N=total number of genes on genome, r = number of gene-set's genes on a phylostratum and n = total number of genes  on phylostratum of interest.
+ 
+=head3 Examples
+
+=over 8
+
+=item B<1. Get taxonomy data>
+
+TODO
+
+
+ 
+=back
+ 
+ 
+ 
+=cut
