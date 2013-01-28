7997e5454f3725fb010dd0954633079024f5ae48
Inti Pedroso <intipedroso@gmail.com>
2013-01-28 11:05:00 +0000

    Major changes - code has been re-ordered. 	* functions has been coded explicitly and moved to the package file 	* Main script contains discrete commands as user would use it on the future.
    Signed-off-by: Inti Pedroso <intipedroso@gmail.com>

--- PhyloStratiphytUtils.py
+++ PhyloStratiphytUtils.py
@@ -6,7 +6,81 @@ __version__ = '0.01'
 
 import datetime
 import pandas as pd
-from numpy import isnan
+import numpy as np
+from Bio import SeqIO
+from cogent.parse.ncbi_taxonomy import NcbiTaxonomyFromFiles
+
+
+# Load species tree from NCBI Taxonomy
+def load_ncbi_taxonomy_tree(nodes,names):
+    print return_time(), "Reading NBCI Taxonomy files"
+    tree = NcbiTaxonomyFromFiles( open(nodes) , open(names))
+    print return_time(), "   ... done"
+    return tree
+
+
+#load gi to tax id mapping
+def load_gi_taxid_mapping(file,table,chunk_size=10000,compression = 'gzip',filter='true'):
+    start_size = len(table)
+    print return_time(), "Reading sequence GI to Taxonomy Id mapping file [", file,"]"
+    gi_2_taxid = pd.read_table(file, chunksize=chunk_size,compression=compression,header=None,sep="\t",names=['gi','taxid'])
+    if filter == 'true':
+        how_filter = 'inner'
+    else:
+        how_filter = 'outer'
+
+    print return_time(), "   '-> Matching blast results GIs with sequences Taxonomy Ids and filtering blast result table"
+    tmp_table = pd.concat([pd.merge(table,chunk,left_on='subject_gi', right_on='gi',how=how_filter) for chunk in gi_2_taxid], ignore_index=True)
+    if filter == 'true':
+            end_size = len(tmp_table)
+            if len(tmp_table) == 0:
+                print return_time(), "Something went wrong. I could map any sequence search result's hit to TaxIds. Check data type of columns with GIs. Returning original table"
+                return table
+            print return_time(), "   '-> Filtered out [", start_size - end_size, "] rows of the table"
+            print return_time(), "   '-> [", end_size, "] blast results had mapable TaxIds"
+
+    print return_time(), "   ... done"
+    return tmp_table
+
+# Read sequences used for the analyses
+def read_fasta_seqs(file):
+    print return_time(), "Reading sequences used for blast search"
+    sequence_length = {}
+    handle = open("example/dysbindin.fa", "rU")
+    for record in SeqIO.parse(handle, "fasta") :
+        sequence_length[record.id] = len(record.seq)
+    handle.close()
+    return sequence_length
+
+
+def filter_table_by_ids(table, ids):
+    print return_time(), "Filtering table"
+    start_size = len(table)
+    table = table[table['query_id'].isin(ids) ]
+    end_size = len(table)
+    print return_time(), "   '-> Filtered out [", start_size - end_size, "] rows of the table"
+    print return_time(), "   ... done"
+    return table
+
+# function to load blast table
+def load_blast_results(file,format='blastp_table'):
+    print return_time(), "Reading sequence search results from [", file, "]"
+    if format == 'blastp_table':
+        colnames = ['query_id', 'subject_id', 'identity', 'alignment_length', 'query_length', 'subject_length', 'mismatches', 'gap_opens', 'q_start', 'q_end', 's_start', 's_end', 'evalue', 'bit_score']
+        table = pd.read_table(file, "r", delimiter="\t", header=None, names = colnames)
+        print return_time(), "   '-> [", len(table), "] results read"
+        # reduce the blast output only to the sequences present on the fasta file
+        #table = table[table['query_id'].isin(sequence_length.keys()) ]
+        table['subject_gi'] = table['subject_id'].str.split('|').apply(lambda x: x[1])
+        table['subject_gi']=table['subject_gi'].astype(np.int64)
+        print return_time(), "   ... done"
+        return table
+
+
+# function to print out a string
+def print_OUT(x):
+    print return_time(), x
+
 
 ## simple function to return time for printing
 def return_time():
@@ -51,12 +125,13 @@ def get_lca(lineage1,lineage2):
             break
 
 # for a list of ids it returns the last common ancestor (and additional information) between the taxid on the list and a refid.
-def get_lca_for_list(coming_in,tree):
+def get_lca_for_list(coming_in,tree,ref_species_id=9606):
+    print return_time(), "Identifying last common ancestor between query specie and blast hits"
     back = {}
-    ref_node = tree.ById[9606]
+    ref_node = tree.ById[ref_species_id]
     ref_lineage = get_lineage(ref_node,tree,'id')
     for number in coming_in:
-        if isnan(number):
+        if np.isnan(number):
             continue
         else:
             sbj_node = tree.ById[number]
@@ -70,4 +145,8 @@ def get_lca_for_list(coming_in,tree):
             #print ref_node.Name," :: ", sbj_node.Name, " => ", qry_sbj_lca_node.Name," <=\n",sbj_node_lineage,"\n",ref_lineage
             back[number] = [sbj_node.Name, number, qry_sbj_lca_node.Name, qry_sbj_lca_id,qry_sbj_lca_rank]
     back = pd.DataFrame(back.values(), columns=['subject_name','subject_taxid','lca_name','lca_tax_id','lca_rank'])
+    print return_time(), "   ... done"
     return back
+
+
+
--- phylostratigraphy.py
+++ phylostratigraphy.py
@@ -3,47 +3,26 @@
 
 from PhyloStratiphytUtils import *
 import pandas as pd
-import numpy as np
-import datetime
-from Bio import SeqIO
-from cogent.parse.ncbi_taxonomy import NcbiTaxonomyFromFiles
 
-# Read sequences used for the analyses
-print return_time(), "Reading sequences used for blast search"
-sequence_length = {}
-handle = open("example/dysbindin.fa", "rU")
-for record in SeqIO.parse(handle, "fasta") :
-    sequence_length[record.id] = len(record.seq)
-handle.close()
 
 
 ## Read blast output results
-print return_time(), "Reading BLAST output file"
-# Define the columns names
-colnames = ['query_id', 'subject_id', 'identity', 'alignment_length', 'query_length', 'subject_length', 'mismatches', 'gap_opens', 'q_start', 'q_end', 's_start', 's_end', 'evalue', 'bit_score']
-# read blast output skipping commented lines
 file1 = 'example/blastp_Pbar_ant_vs_nr_e10.txt_nohash'
 file2 = 'example/dysbindin.blast_out.txt'
-table = pd.read_table(file1, "r", delimiter="\t", header=None, names = colnames)
-print return_time(), "   '-> [", len(table), "] results read"
-# reduce the blast output only to the sequences present on the fasta file
-#table = table[table['query_id'].isin(sequence_length.keys()) ]
-table['subject_gi'] = table['subject_id'].str.split('|').apply(lambda x: x[1])
-table['subject_gi']=table['subject_gi'].astype(np.int64)
+table = load_blast_results(file2)
 
-print return_time(), "Reading sequence GI to Taxonomy Id mapping file"
-tp = pd.read_table('gi_taxid_prot.dmp.gz', chunksize=10000,compression="gzip",header=None,sep="\t",names=['gi','taxid'])
+sequence_length = read_fasta_seqs('example/dysbindin.fa')
+table = filter_table_by_ids(table,sequence_length.keys())
 
-print return_time(), "   '-> Matching blast results GIs with sequences Taxonomy Ids"
-gimap = pd.concat([pd.merge(table,chunk,left_on='subject_gi', right_on='gi',how="inner") for chunk in tp], ignore_index=True)
-print return_time(), "   '-> [", len(gimap), "] BLAST hits with identified taxid"
+table = load_gi_taxid_mapping('gi_taxid_prot.dmp.gz',table)
+#table = load_gi_taxid_mapping('head.txt.gz',table)
 
-print return_time(), "Reading NBCI Taxonomy files"
-tree = NcbiTaxonomyFromFiles(open('/Users/inti/Installed_Apps/biosql/scripts/taxdata/nodes.dmp'), open('/Users/inti/Installed_Apps/biosql/scripts/taxdata/names.dmp'))
+tree = load_ncbi_taxonomy_tree('/Users/inti/Installed_Apps/biosql/scripts/taxdata/nodes.dmp','/Users/inti/Installed_Apps/biosql/scripts/taxdata/names.dmp')
 
-print return_time(), "Identifying last common ancestor between query specie and blast hits"
-lcas = get_lca_for_list(list(set(gimap['taxid'])),tree)
-gimap2 = pd.merge(gimap,lcas,left_on='taxid',right_on='subject_taxid', how='inner')
-print gimap2[gimap2.columns[[0,1,15,16,17,19,20,21]]][:5]
+lcas = get_lca_for_list(list(set(table['taxid'])),tree)
+table = pd.merge(table,lcas,left_on='taxid',right_on='subject_taxid', how='inner')
+
+
+table.ix[(table['evalue'] < 1e-3) & (table['lca_rank'] == min(table['lca_rank'])),'lca_name' ]
 
 exit(0)
