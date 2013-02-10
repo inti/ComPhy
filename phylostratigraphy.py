1a79d9c9078e5aec448bac58747d1a548c7d78e1
Inti Pedroso <intipedroso@gmail.com>
2013-02-10 22:50:11 +0000

    start plots

--- phylostratigraphy.py
+++ phylostratigraphy.py
@@ -9,21 +9,16 @@ p_barbatus_taxid = 144034
 d_mell_taxid = 7227
 h_sapiens_taxid = 9606
 evalue_cutoff = 1e-3
-ref_species = p_barbatus_taxid
+ref_species = d_mell_taxid
 
 # Load taxonomy information
 tax_nodes, tax_names = ttutils.load_ncbi_tax_files('ncbi_tax_data/nodes.dmp','ncbi_tax_data/names.dmp')
-
-
-#h5_store = store_gi_to_taxid_mapping_to_h5(gi2taxid='gi_taxid_prot.dmp.gz')
-#gi_2_taxid_table = h5_store.root.gi_2_taxid.table
-
 
 ## Read blast output results
 file1 = 'example/blastp_Pbar_ant_vs_nr_e10.txt_nohash'
 file2 = 'dysbindin.blast_out.txt_nohash'
 file3 = 'dmel-all-translation-r5.44.fasta.nr.e10.sw.txt_nohash'
-table = psutils.load_blast_results(file3,evalue_limit=evalue_cutoff)
+table = psutils.load_blast_results(file1,evalue_limit=evalue_cutoff)
 
 psutils.store_gi_to_taxid_mapping_to_h5(gi2taxid='gi_taxid_prot.dmp.gz',store_file='gi_taxid.h5', close=True,override=False)
 df_unique_gi = psutils.add_taxid_from_h5file(table = table,file = 'gi_taxid.h5')
@@ -39,5 +34,12 @@ table_mrca = psutils.map_to_oldest_stratum(table)
 phylostratum_counts = psutils.count_genes_per_phylostrata(table_mrca,tax_nodes, tax_names,ref_species_id=ref_species)
 print phylostratum_counts
 b = psutils.bootstrap(table,B=100)
-b
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+#ax.plot(phylostratum_counts['rank'],phylostratum_counts['phylostratum'])
+ax.errorbar(phylostratum_counts['rank'],phylostratum_counts['phylostratum'],yerr=b[['q2.5','q97.5']].values)
+plt.show()
 
